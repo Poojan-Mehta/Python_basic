647eabf955d1d12f1163b922c706f8e7657718c4
poojan <poojanmehta008@gmail.com>
2023-09-11 21:14:10 +0530

    master first commit

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+print("hello there please provide me some of your details")
+
+firstname = input("hello enter your firstname: ")
+
+user_details = []
+if(firstname == ''):
+	print("Please fill the form")
+	input("hello enter your firstname: ")
+else:
+	print(firstname)
+
